b166225e56bc6bd3b33fcd088fad8de7520d96be
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-13 19:39:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
    return combo if combo.all?{|x| board[x] == 'X'}
+   return combo if combo.all?{|x| board[x] == 'O'}
  end
  false
  end
